c3911763b6b18e24529c8c430a20a9c82e415b47
xxfariddx <qarayevfarid3@gmail.com>
2026-03-17 11:41:40 +0000

    ifelse

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(s):
+    for c in s:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
