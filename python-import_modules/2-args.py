943b760c4b8398c9e10a6278a616b4356e5af7d8
xxfariddx <qarayevfarid3@gmail.com>
2026-03-22 09:13:35 +0000

    python-import_modules

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     elif argc == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(argc))
+        print(f"{argc} arguments:")
 
     for i in range(1, len(argv)):
-        print("{}: {}".format(i, argv[i]))
+        print(f"{i}: {argv[i]}")
