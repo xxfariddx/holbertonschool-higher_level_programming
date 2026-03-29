26398ed233a915173bb72cfa0aadba91a649fe7b
xxfariddx <qarayevfarid3@gmail.com>
2026-03-29 09:37:38 +0000

    python-more_data_structures

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+
+    for row in matrix:
+        new_row = []
+        for num in row:
+            new_row.append(num ** 2)
+        new_matrix.append(new_row)
+
+    return new_matrix
