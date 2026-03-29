b0601b92919a855d8a0928037741332516c023e1
xxfariddx <qarayevfarid3@gmail.com>
2026-03-29 10:56:51 +0000

    python-exceptions

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
+def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
+        # Burada IndexError tutulmur ki, x list-dən böyük olsa traceback çıxsın
         try:
-            print(my_list[i], end="")
+            print("{:d}".format(my_list[i]), end="")
             count += 1
-        except Exception:
+        except (ValueError, TypeError):
             continue
     print()
     return count
