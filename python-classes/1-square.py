cf4c72e46c0d8603aeda9d54fc715d5a9c0111e9
xxfariddx <qarayevfarid3@gmail.com>
2026-04-03 15:26:40 +0000

    pythonclasses

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+        """Initialize square with size."""
+        self.__size = size
