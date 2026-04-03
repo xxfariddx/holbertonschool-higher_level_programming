4351f2346d2b30cdb035968903311b0ed97cbbb5
xxfariddx <qarayevfarid3@gmail.com>
2026-04-03 15:42:48 +0000

    pythonclasses

---
+++ python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        """Initialize square with optional size."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
