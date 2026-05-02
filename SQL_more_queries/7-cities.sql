900a7c3d5aa96b510f917f4e97335246b095e12a
xxfariddx <qarayevfarid3@gmail.com>
2026-05-02 14:32:26 +0400

    SQL_more_queries

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,21 +1,14 @@
-/*
-Write a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
-
-    cities description:
-    id INT unique, auto generated, can't be null and is a primary key
-    state_id INT, can't be null and must be a FOREIGN KEY that references to id of the states table
-    name VARCHAR(256) can't be null
-    If the database hbtn_0d_usa already exists, your script should not fail
-    If the table cities already exists, your script should not fail
-*/
 
 
+-- Creates the database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Use the database hbtn_0d_usa
 USE hbtn_0d_usa;
 
+-- Creates the table cities
 CREATE TABLE IF NOT EXISTS cities (
-    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    FOREIGN KEY(state_id) REFERENCES states(id)
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, -- ID column
+    state_id INT NOT NULL, -- State ID column
+    name VARCHAR(256) NOT NULL, -- Name column
+    FOREIGN KEY(state_id) REFERENCES states(id) -- Foreign key to states table
 );
