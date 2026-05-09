ef99b707a0f95ae1945ae62239b3aef5200a1f4a
xxfariddx <qarayevfarid3@gmail.com>
2026-05-09 13:54:56 +0400

    javascript-warm_up

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-if (process.argv.length <= 2) {
-	console.log('No argument');
-} else if (process.argv.length === 3) {
-	console.log('Argument found');
+
+const arg = process.argv.slice(2);
+if (arg.length === 0) {
+  console.log('No argument');
+} else if (arg.length === 1) {
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
